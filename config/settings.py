43c7beaae84ca5eb07c73207c0159f11d3aa8ab8
John <jclry76@gmail.com>
2021-08-03 21:49:39 +0100

    Set the user model to accounts.CustomUser. Add accounts to installed apps

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # MS4 Apps
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -129,3 +132,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Use CustomUser
+AUTH_USER_MODEL = 'accounts.CustomUser'
