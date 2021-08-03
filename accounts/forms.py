4c33a91303c7b53103e6a22c64daa10474b60b4f
John <jclry76@gmail.com>
2021-08-03 22:01:48 +0100

    Set up CustomUser user model

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,13 +1,13 @@
 from django.contrib.auth import get_user_model 
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
-# Setup based on Django for Professionals
+# Setup based on Django for Professionals with some additional fields
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = get_user_model()
-        fields = ('email', 'username',)
+        fields = ('email', 'username', 'is_staff', 'last_login')
     
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = get_user_model()
-        fields = ('email', 'username',)
+        fields = ('email', 'username', 'is_staff', 'last_login')
---
+++ accounts/models.py
@@ -0,0 +1,6 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+# Custom user based on Django for Professinals and Django docs 
+class CustomUser(AbstractUser):
+    pass
