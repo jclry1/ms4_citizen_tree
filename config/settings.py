6e6d07b256eddb3edd1c9a4541e5243f58e1abd3
John <jclry76@gmail.com>
2021-08-03 22:40:00 +0100

    Update settings for static files - DIRS, ROOT, FINDERS

--- config/settings.py
+++ config/settings.py
@@ -127,6 +127,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+# Static setup based on Django for Professionals
+STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),)
+STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
+STATICFILES_FINDERS = [ 
+"django.contrib.staticfiles.finders.FileSystemFinder",
+"django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
