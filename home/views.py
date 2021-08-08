9164ff5dcd7251382b84141465d97a89c0044712
John <jclry76@gmail.com>
2021-08-08 13:04:45 +0100

    Set up and test using class-based view and pass context from view to template

--- home/views.py
+++ home/views.py
@@ -1,5 +1,17 @@
 # Setup based on Django for Professionals
+# Understanding of class-based view from: https://www.youtube.com/watch?v=GxA2I-n8NR8
+
 from django.views.generic import TemplateView
+from accounts.models import CustomUser
+
 
 class HomePageView(TemplateView):
     template_name = 'home/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['heading'] = "My heading for the home page"
+        if self.request.user.is_authenticated:
+            user = CustomUser.objects.get(pk=self.request.user.pk)
+            context ['username'] = user.username
+        return context
