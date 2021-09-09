3f3b9afd5ddb66f0f8e5c4421dc600bb65967d54
John <jclry76@gmail.com>
2021-09-09 23:29:36 +0100

    Add url path for project-specific updates page

--- projects/urls.py
+++ projects/urls.py
@@ -1,3 +1,4 @@
+
 from django.urls import path
 from . import views
 
@@ -8,4 +9,5 @@ urlpatterns = [
     path('updates/', views.UpdatesView.as_view(), name = 'updates'),
     path('reports/<int:pk>/', views.ReportView.as_view(), name='report'),
     path('<slug:slug>/', views.SpotlightView.as_view(), name='spotlight'),
+    path('updates/<project>/', views.ProjectUpdatesView.as_view(), name='projupdates'),
     ]
