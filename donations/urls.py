9c8a98133bed34d22dd1afa2fafe39f889473fae
John <jclry76@gmail.com>
2021-09-12 00:27:54 +0100

    Add initial urls for strip setup

--- donations/urls.py
+++ donations/urls.py
@@ -5,7 +5,8 @@ from . import views
 app_name = 'donations'
 
 urlpatterns = [
-    path('', views.DonateInfo, name="donateinfo"),
+    path('', views.DonateLandingPage.as_view(), name="donate-landing"),
+    path('create-checkout-session/', views.CreateCheckoutSessionView.as_view(), name='create-checkout-session')
     #path('donate/',views.donate, name = "donate"),
     #path('success/<str:args>', views.successMsg, name = "success"),
 ]
