1912fca33ff2066a3319423592eeb8fd335700bb
John <jclry76@gmail.com>
2021-09-12 16:48:16 +0100

    Debug webhook issue with new code from stripe - unresolved

--- donations/views.py
+++ donations/views.py
@@ -1,18 +1,18 @@
 import stripe
-from django.http import HttpResponse
+from django.core.mail import send_mail
+from django.http import HttpResponse, JsonResponse
 from django.conf import settings
 from django.views.generic import TemplateView
 from django.views.decorators.csrf import csrf_exempt
-from django.http import JsonResponse
 from django.shortcuts import redirect
 from django.views import View
 
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
-endpoint_secret = settings.STRIPE_WH_SECRET
 
 
-class DonateLandingPage(TemplateView):              # This works
+
+class DonateLandingPage(TemplateView):            
     template_name = "donations/donate.html"
 
 
@@ -37,23 +37,88 @@ def create_checkout_session(request):
         payment_method_types=[
             'card',
         ],
+        metadata={
+            "placeholder":"placeholder data"
+        },
         mode='payment',
         success_url= DOMAIN + 'success/',
         cancel_url= DOMAIN + 'cancel/',
     )
     return redirect(checkout_session.url, code=303)
 
-
-
 # From: https://stripe.com/docs/payments/checkout/fulfill-orders
-
 
 @csrf_exempt
 def stripe_webhook(request):
+  payload = request.body
+
+  # For now, you only need to print out the webhook payload so you can see
+  # the structure.
+  print(payload)
+
+  return HttpResponse(status=200)
+
+
+
+""" @csrf_exempt
+def stripe_webhook(request):
+  endpoint_secret = settings.STRIPE_WH_SECRET
+  payload = request.body
+  sig_header = request.META['HTTP_STRIPE_SIGNATURE']
+  event = None
+
+  try:
+    event = stripe.Webhook.construct_event(
+      payload, sig_header, endpoint_secret
+    )
+  except ValueError as e:
+    # Invalid payload
+    return HttpResponse(status=400)
+  except stripe.error.SignatureVerificationError as e:
+    # Invalid signature
+    return HttpResponse(status=400)
+
+  # Handle the checkout.session.completed event
+  if event['type'] == 'checkout.session.completed':
+    session = event['data']['object']
+
+    print(session)
+
+  # Passed signature verification
+  return HttpResponse(status=200) """
+
+
+
+""" def stripe_webhook(request):
+    endpoint_secret = settings.STRIPE_WH_SECRET
     payload = request.body
+    sig_header = request.META['HTTP_STRIPE_SIGNATURE']
+    event = None
 
-    # For now, you only need to print out the webhook payload so you can see
-    # the structure.
-    print(payload)
+    try:
+        event = stripe.Webhook.construct_event(
+          payload, sig_header, endpoint_secret
+        )
+    except ValueError as e:
+      # Invalid payload
+        return HttpResponse(status=400)
+    except stripe.error.SignatureVerificationError as e:
+      # Invalid signature
+        return HttpResponse(status=400)
 
-    return HttpResponse(status=200)
+    # Handle the checkout.session.completed event
+    if event['type'] == 'checkout.session.completed':
+        session = event['data']['object']
+
+        customer_email = session["customer_details"]["email"]
+        #product_info = session["metadata"]["placeholder"]
+
+        print(session["customer_details"])
+
+        send_mail(
+            subject="Thanks for your donation",
+            message=f"Thanks for your donation of xxx",
+            recipient_list=[customer_email],
+            from_email="admin@citizentree.com"
+        )
+    return HttpResponse(status=200) """
