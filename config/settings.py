ec289e0ee6676d499415a31328ad9c8ed3ce76e5
John <jclry76@gmail.com>
2021-09-12 13:55:53 +0100

    Add stripe wh secret

--- config/settings.py
+++ config/settings.py
@@ -175,5 +175,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' # Use console t
 # Stripe
 STRIPE_SECRET_KEY = env('STRIPE_SECRET_KEY')
 #STRIPE_PUBLIC_KEY = env('STRIPE_PUBLIC_KEY')
-#STRIPE_WH_SECRET = env('STRIPE_WH_SECRET')
+STRIPE_WH_SECRET = env('STRIPE_WH_SECRET')
 #STRIPE_CURRENCY = 'eur'
