033833124f201894a743aadba09fdf57f431fd55
John <jclry76@gmail.com>
2021-09-12 12:56:15 +0100

    Add functions to create checkout session, show success, show cancel pages

--- donations/views.py
+++ donations/views.py
@@ -2,39 +2,40 @@ import stripe
 from django.conf import settings
 from django.views.generic import TemplateView
 from django.http import JsonResponse
-from django.shortcuts import render
+from django.shortcuts import redirect
 from django.views import View
-import os
+
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
-def DonateInfo(request):
-    return render(request, 'donations/donate.html')
 
-class DonateLandingPage(TemplateView):
+class DonateLandingPage(TemplateView):              # This works
     template_name = "donations/donate.html"
 
 
-class CreateCheckoutSessionView(View):
-    def post(self, request, *args, **kwargs):
-        YOUR_DOMAIN = 'http://127.0.0.1:8000/donations/'
+class SuccessView(TemplateView):
+    template_name = "donations/success.html"
 
-        checkout_session = stripe.checkout.Session.create(
-            line_items=[
-                {
-                    'price': '{{PRICE_ID}}',
-                    'quantity': 1,
-                },
-            ],
-            payment_method_types=[
-              'card',
-            ],
-            mode='payment',
-            success_url=YOUR_DOMAIN + '/success.html',
-            cancel_url=YOUR_DOMAIN + '/cancel.html',
-        )
-        return JsonResponse(
+
+class CancelView(TemplateView):
+    template_name = "donations/cancel.html"
+
+
+def create_checkout_session(request):
+    DOMAIN = 'http://127.0.0.1:8000/donations/'
+    
+    checkout_session = stripe.checkout.Session.create(
+        line_items=[
             {
-                'id': checkout_session.id
-            }
-        )    
+                'price': 'price_1JYnvKGXFBy9KvEyU8nAhWTf',
+                'quantity': 1,
+            },
+        ],
+        payment_method_types=[
+            'card',
+        ],
+        mode='payment',
+        success_url= DOMAIN + 'success/',
+        cancel_url= DOMAIN + 'cancel/',
+    )
+    return redirect(checkout_session.url, code=303)
