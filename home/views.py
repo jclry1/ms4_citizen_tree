78cf815ee234a45268c941a88760b0892eabaab8
John <jclry76@gmail.com>
2021-09-14 00:19:01 +0100

    Add temporary sendmail function to test sendgrid integraiton

--- home/views.py
+++ home/views.py
@@ -3,6 +3,7 @@
 
 from django.views.generic import TemplateView
 from accounts.models import CustomUser
+from django.core.mail import send_mail
 
 
 class HomePageView(TemplateView):
@@ -14,4 +15,10 @@ class HomePageView(TemplateView):
         if self.request.user.is_authenticated:
             user = CustomUser.objects.get(pk=self.request.user.pk)
             context ['username'] = user.username
+        
+        """ 
+        Test sendgrid integration:
+        send_mail('Subject here', 'Here is the message.', 'ms4.citizentree@gmail.com', ['jclry76@gmail.com'], fail_silently=False) """
+        print('hello')
+
         return context
