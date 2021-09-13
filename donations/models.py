1108b8d0ef3200d1f12b467d66f54b432f3fcf27
John <jclry76@gmail.com>
2021-09-13 07:07:09 +0100

    Add models Donation and Price

--- donations/models.py
+++ donations/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+# Based on: https://justdjango.com/blog/django-stripe-payments-tutorial
+
+class Donation(models.Model):
+    name = models.CharField(max_length=100)
+    stripe_product_id = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+    
+
+class Price(models.Model):
+    donation = models.ForeignKey(Donation, on_delete=models.CASCADE)
+    description = models.CharField(max_length=100, default = "desc")
+    stripe_price_id = models.CharField(max_length=100)
+    price = models.IntegerField(default=0)  #Note: Cents value
+
+    def __str__(self):
+        return self.description
+	
+    def get_display_price(self):
+        return "{0:.2f}".format(self.price / 100) #Convert from cents to euros for display
