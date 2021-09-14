5fcfbf2ee2b264a9089d9f279048f5a892a8c354
John <jclry76@gmail.com>
2021-09-14 23:02:10 +0100

    Remove trailing slash from webhook url to fix 500 error (stripe)

--- donations/urls.py
+++ donations/urls.py
@@ -16,5 +16,5 @@ urlpatterns = [
     path('create-checkout-session/<pk>/', CreateCheckoutSessionView.as_view(), name='create-checkout-session'),
     path('/cancel/', CancelView.as_view(), name='cancel'),
     path('/success/', SuccessView.as_view(), name='success'),
-    path('/webhooks/stripe/', stripe_webhook, name='stripe-webhook'),
+    path('/webhooks/stripe', stripe_webhook, name='stripe-webhook'),
 ]
