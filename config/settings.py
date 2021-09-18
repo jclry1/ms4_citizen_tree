64537a9d5ca1c5474c5d640fada760cee2e68791
John <jclry76@gmail.com>
2021-09-19 00:01:49 +0100

    Add default setting for django debug or env setting if is one

--- config/settings.py
+++ config/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = env.bool("DJANGO_DEBUG", default=False)
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
