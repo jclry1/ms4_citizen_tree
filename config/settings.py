f77698bbb68b97bd10fd4c155b42a3f4b8445ba0
John <jclry76@gmail.com>
2021-09-18 06:48:14 +0100

    Make secret key point to env, with new secret

--- config/settings.py
+++ config/settings.py
@@ -12,7 +12,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=)e#qx!7h4cd0$vhzp7$qj*-o_gt$uyf@fk&nnf)on0qt=610b'
+
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
