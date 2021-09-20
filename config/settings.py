2439e74ccc0b671fab845f00ea407b9f8244b0dc
John <jclry76@gmail.com>
2021-09-20 10:09:25 +0100

    Set debug = true to debug 500 error

--- config/settings.py
+++ config/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool("DJANGO_DEBUG", default=False)
+DEBUG = env.bool("DJANGO_DEBUG", default=True)
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com']
 
 #SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https') # Fix 'redirected too  many times error' on Heroku
