5961650b4b1cf58baed92ff79af7358a1cfa8cb6
John <jclry76@gmail.com>
2021-09-21 21:40:21 +0100

    Remove leading slash for cancel, success, and webhooks to part-fix 500 err in heroku when debug=false

--- donations/urls.py
+++ donations/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path('', DonateLandingPage.as_view(), name="donate-landing"),
     #path('donations/create-checkout-session/', views.create_checkout_session, name ="create-checkout-session"),
     path('create-checkout-session/<pk>/', CreateCheckoutSessionView.as_view(), name='create-checkout-session'),
-    path('/cancel/', CancelView.as_view(), name='cancel'),
-    path('/success/', SuccessView.as_view(), name='success'),
-    path('/webhooks/stripe', stripe_webhook, name='stripe-webhook'),
+    path('cancel/', CancelView.as_view(), name='cancel'),
+    path('success/', SuccessView.as_view(), name='success'),
+    path('webhooks/stripe', stripe_webhook, name='stripe-webhook'),
 ]
