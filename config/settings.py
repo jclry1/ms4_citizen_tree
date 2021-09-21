2233fc32fe7174c5edb06aa577929065fa2b068e
John <jclry76@gmail.com>
2021-09-21 22:25:59 +0100

    Add logging settings for debugging heroku

--- config/settings.py
+++ config/settings.py
@@ -233,3 +233,42 @@ try:
     from .local_settings import *
 except ImportError:
     pass
+
+
+# Logging settings to help debug 500 error when deployed with debug = false
+# https://stackoverflow.com/questions/52311724/500-error-when-debug-false-with-heroku-and-django
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': ('%(asctime)s [%(process)d] [%(levelname)s] ' +
+                       'pathname=%(pathname)s lineno=%(lineno)s ' +
+                       'funcname=%(funcName)s %(message)s'),
+            'datefmt': '%Y-%m-%d %H:%M:%S'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        }
+    },
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'logging.NullHandler',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'testlogger': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        }
+    }
+}
+
+DEBUG_PROPAGATE_EXCEPTIONS = True
