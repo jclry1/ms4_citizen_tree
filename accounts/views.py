e10332b5b2085a4b87e9a13ecbfee1d709cb754c
John <jclry76@gmail.com>
2021-09-24 23:08:02 +0100

    Update signup to use allauth signup view

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,11 @@
 # Setup based on Django for Professionals
-
+from allauth.account.views import SignupView # https://stackoverflow.com/questions/62039143/django-allauth-confirmation-verification-email-not-sent-after-signing-up
 from django.urls import reverse_lazy
 from django.views import generic
 from .forms import CustomUserCreationForm
 
 # Create your views here.
-class SignupPageView(generic.CreateView):
+class SignupPageView(SignupView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
