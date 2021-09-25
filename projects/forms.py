d059d7725f693cbe8cf3b04a3f2d25d924460ebe
John <jclry76@gmail.com>
2021-09-25 23:07:19 +0100

    Add form to update project details

--- projects/forms.py
+++ projects/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Update
+from .models import Update, Project
 
 
 class AddUpdate(forms.ModelForm):
@@ -13,3 +13,12 @@ class AddUpdate(forms.ModelForm):
             'text': forms.Textarea(attrs={'class': 'form-control'}),
             'short_text': forms.TextInput(attrs={'class': 'form-control'}),
         } 
+
+
+#Form for updating the project details
+class EditProject(forms.ModelForm):
+
+    class Meta:
+        model = Project
+        fields = ('title', 'description', 'author', 'location', 'nursery', 'plantation',
+                'species', 'available_to_partner', 'area_ha', 'started', 'picture')
