7065e43effe1d29ddabb8512f39659b64236f04f
John <jclry76@gmail.com>
2021-09-26 23:22:48 +0100

    Toggle debug to test custom 404

--- config/settings.py
+++ config/settings.py
@@ -20,6 +20,8 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DJANGO_DEBUG", default=True)
+#DEBUG = False
+
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com']
 
 #SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https') # Fix 'redirected too  many times error' on Heroku
