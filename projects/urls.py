8102dcfc6fc44f9f0bb7c81e940b8b6b07b85e88
John <jclry76@gmail.com>
2021-09-26 10:39:39 +0100

    Add path for deleting update

--- projects/urls.py
+++ projects/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('updates/', views.UpdatesView.as_view(), name = 'updates'),
     path('updates/add/<slug:slug>', views.AddUpdateView.as_view(), name='add-update'),
     path('reports/<int:pk>/', views.ReportView.as_view(), name='report'),
+    path('reports/<int:pk>/delete', views.DeleteReportView.as_view(), name='report-delete'),
     path('updates/<project>/', views.ProjectUpdatesView.as_view(), name='projupdates'),
     path('userupdates/', views.UserUpdatesView.as_view(), name='userupdates'),
     path('<slug:slug>/', views.SpotlightView.as_view(), name='spotlight'),
