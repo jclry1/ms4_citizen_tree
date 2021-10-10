f8b85ca3a16cfbac2f4b34ea0a3924ad5eb77448
John <jclry76@gmail.com>
2021-10-10 20:02:39 +0100

    Add the Donor model to admin

--- donations/admin.py
+++ donations/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from .models import Donation, Price
+from .models import Donation, Price, Donor
 
 admin.site.register(Donation) #Make project model available in Admin
 
 admin.site.register(Price) #Make project model available in Admin
+
+admin.site.register(Donor) #Make project model available in Admin
