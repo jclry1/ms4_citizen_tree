15d55d16175586934423e78147ddbb0729939ec0
John <jclry76@gmail.com>
2021-10-13 22:31:46 +0100

    Redo code from chart.js usage guide

--- static/js/pie.js
+++ static/js/pie.js
@@ -1,4 +1,48 @@
+// https://www.chartjs.org/docs/latest/getting-started/usage.html
+// https://www.chartjs.org/docs/latest/configuration/legend.html
+// https://stackoverflow.com/questions/37292423/chart-js-label-color
+
+
+var ctx = document.getElementById('co2_doughnut');
 let total_donor_sink = JSON.parse(document.getElementById('d_sink_chart').textContent);
+let chart_sink_kg = total_donor_sink.donor_sink;
+let chart_sink_tonne = parseInt(chart_sink_kg / 1000);
+
+var myChart = new Chart(ctx, {
+  type: 'doughnut',
+  data: {
+      labels: [
+        'Ireland Emissions - Annual (Tonnes)',
+        'Citizen Tree Sink - Annual (Tonnes)',
+      ],
+      datasets: [{
+        label: 'Citizen Tree Sequestration',
+        data: [60000000, chart_sink_tonne],
+        backgroundColor: [
+          'rgb(250, 250, 250)',
+          'rgb(100, 0, 0)',
+        ],
+        hoverOffset: 4,
+        cutout: '75%',
+      }]
+  },
+  options: {
+    plugins: {
+      legend: {
+        display: true,
+        labels: {
+          color: "white",
+          font: {
+            size: 20,
+          }
+        }
+      }
+    }
+  }
+});
+
+
+/* let total_donor_sink = JSON.parse(document.getElementById('d_sink_chart').textContent);
 let chart_sink_kg = total_donor_sink.donor_sink;
 let chart_sink_tonne = parseInt(chart_sink_kg / 1000);
 
@@ -18,6 +62,7 @@ let data = {
     cutout: '75%',
   }],
 };
+// https://www.chartjs.org/docs/latest/getting-started/usage.html
 // https://www.chartjs.org/docs/latest/configuration/legend.html
 // https://stackoverflow.com/questions/37292423/chart-js-label-color
 let config = {
@@ -41,4 +86,4 @@ let config = {
 let myChart = new Chart(
   document.getElementById('co2_doughnut'),
   config
-);
+); */
