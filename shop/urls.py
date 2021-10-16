10c77605c34a2814b56390bed9b9855aaebddf1c
John <jclry76@gmail.com>
2021-10-17 00:06:12 +0100

    add urls for handling increasin descreasing an removing items from teh cart

--- shop/urls.py
+++ shop/urls.py
@@ -6,5 +6,8 @@ app_name = 'shop'
 urlpatterns = [
     path('', views.CartView.as_view(), name='summary'),
     path('shop/', views.ProductListView.as_view(), name='product-list'),
-    path('shop/<slug>/', views.ProductDetailView.as_view(), name='product-spotlight')
+    path('shop/<slug>/', views.ProductDetailView.as_view(), name='product-spotlight'),
+    path('increase-quantity/<pk>/', views.IncreaseQuantityView.as_view(), name = 'increase-qty'),
+    path('decrease-quantity/<pk>/', views.DecreaseQuantityView.as_view(), name = 'decrease-qty'),
+    path('remove-from-cart/<pk>/', views.RemoveFromCartView.as_view(), name = 'remove-item'),
 ]
