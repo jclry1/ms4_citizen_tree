9e40aa46fafe9e1a62311806a287a436dd195c9b
John <jclry76@gmail.com>
2021-10-16 10:53:51 +0100

    Add variety-select option for the form

--- shop/forms.py
+++ shop/forms.py
@@ -1,10 +1,17 @@
 from django import forms
-from .models import OrderItem
+from .models import OrderItem, VarietySpec, Product
 
 
 class AddToCartForm(forms.ModelForm):
-
+    variety = forms.ModelChoiceField(queryset=VarietySpec.objects.none())
 
     class Meta:
         model = OrderItem
-        fields = OrderItemfields = ['quantity']
+        fields = OrderItemfields = ['quantity', 'variety']
+
+    def __init__(self, *args, **kwargs):
+        product_id = kwargs.pop('product_id')
+        product = Product.objects.get(id=product_id)
+        super().__init__(*args, **kwargs)
+
+        self.fields['variety'].queryset = product.available_varieties.all()
