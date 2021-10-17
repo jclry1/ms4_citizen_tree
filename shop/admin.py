10340ae624aafae1a2f8cdbc433394bd69f4c880
John <jclry76@gmail.com>
2021-10-17 13:11:46 +0100

    Add addresses and addresses admin

--- shop/admin.py
+++ shop/admin.py
@@ -1,10 +1,22 @@
 from django.contrib import admin
+from django.contrib.admin.options import ModelAdmin
 
 from .models import (
-    Product, Order, OrderItem, VarietySpec)
+    Product, Order, OrderItem, VarietySpec, Address)
+
+class AddressAdmin(admin.ModelAdmin):
+    list_display=[
+        'user',
+        'address_type',
+        'address_line_1',
+        'address_line_2',
+        'county',
+        'eircode',
+    ]
 
 admin.site.register(Product)
 admin.site.register(Order)
 admin.site.register(OrderItem)
 admin.site.register(VarietySpec)
+admin.site.register(Address, AddressAdmin)
 
