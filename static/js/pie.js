8fba15f797711de338a92d24cd171fe4719099d1
John <jclry76@gmail.com>
2021-10-21 22:22:09 +0100

    Put chart in functin and add timeout to aloow the value to be got from template - testing solution

--- static/js/pie.js
+++ static/js/pie.js
@@ -3,43 +3,43 @@
 // https://stackoverflow.com/questions/37292423/chart-js-label-color
 
 
+function chart(){
 var ctx = document.getElementById('co2_doughnut');
 let total_donor_sink = JSON.parse(document.getElementById('d_sink_chart').textContent);
 let chart_sink_kg = total_donor_sink.donor_sink;
 let chart_sink_tonne = parseInt(chart_sink_kg / 1000);
-
-setInterval(myChart, 3000); // Set to 3 seconds
-
-
-var myChart = new Chart(ctx, {
-  type: 'doughnut',
-  data: {
-      labels: [
-        'Ireland Emissions - Annual (Tonnes)',
-        'Citizen Tree Sink - Annual (Tonnes)',
-      ],
-      datasets: [{
-        label: 'Citizen Tree Sequestration',
-        data: [60000000, chart_sink_tonne],
-        backgroundColor: [
-          'rgb(250, 250, 250)',
-          'rgb(100, 0, 0)',
+  var myChart = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+        labels: [
+          'Ireland Emissions - Annual (Tonnes)',
+          'Citizen Tree Sink - Annual (Tonnes)',
         ],
-        hoverOffset: 4,
-        cutout: '75%',
-      }]
-  },
-  options: {
-    plugins: {
-      legend: {
-        display: true,
-        labels: {
-          color: "white",
-          font: {
-            size: 20,
+        datasets: [{
+          label: 'Citizen Tree Sequestration',
+          data: [60000000, chart_sink_tonne],
+          backgroundColor: [
+            'rgb(250, 250, 250)',
+            'rgb(100, 0, 0)',
+          ],
+          hoverOffset: 4,
+          cutout: '75%',
+        }]
+    },
+    options: {
+      plugins: {
+        legend: {
+          display: true,
+          labels: {
+            color: "white",
+            font: {
+              size: 20,
+            }
           }
         }
       }
     }
-  }
-});
+  });
+
+}
+setTimeout(() => chart(), 1500); //Test - Give time for page content to load so that the function can get the value from template
