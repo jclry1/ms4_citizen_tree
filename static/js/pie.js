9bb7590305a2775465643ee1e77aaf9b691c7602
John <jclry76@gmail.com>
2021-10-21 21:41:35 +0100

    Add setInterval to allow js to get content from page - test

--- static/js/pie.js
+++ static/js/pie.js
@@ -8,6 +8,9 @@ let total_donor_sink = JSON.parse(document.getElementById('d_sink_chart').textCo
 let chart_sink_kg = total_donor_sink.donor_sink;
 let chart_sink_tonne = parseInt(chart_sink_kg / 1000);
 
+setInterval(myChart, 3000); // Set to 3 seconds
+
+
 var myChart = new Chart(ctx, {
   type: 'doughnut',
   data: {
@@ -40,50 +43,3 @@ var myChart = new Chart(ctx, {
     }
   }
 });
-
-
-/* let total_donor_sink = JSON.parse(document.getElementById('d_sink_chart').textContent);
-let chart_sink_kg = total_donor_sink.donor_sink;
-let chart_sink_tonne = parseInt(chart_sink_kg / 1000);
-
-let data = {
-  labels: [
-    'Ireland Emissions - Annual (Tonnes)',
-    'Citizen Tree Sink - Annual (Tonnes)',
-  ],
-  datasets: [{
-    label: 'Citizen Tree Sequestration',
-    data: [60000000, chart_sink_tonne],
-    backgroundColor: [
-      'rgb(250, 250, 250)',
-      'rgb(100, 0, 0)',
-    ],
-    hoverOffset: 4,
-    cutout: '75%',
-  }],
-};
-// https://www.chartjs.org/docs/latest/getting-started/usage.html
-// https://www.chartjs.org/docs/latest/configuration/legend.html
-// https://stackoverflow.com/questions/37292423/chart-js-label-color
-let config = {
-  type: 'doughnut',
-  data: data,
-  options: {
-    plugins: {
-      legend: {
-        display: true,
-        labels: {
-          color: "white",
-          font: {
-            size: 20,
-          }
-        }
-      }
-    }
-  }
-};
-
-let myChart = new Chart(
-  document.getElementById('co2_doughnut'),
-  config
-); */
