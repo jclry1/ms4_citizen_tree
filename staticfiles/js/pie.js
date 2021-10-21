0d8417b3514d7fd8a6158d395f0b2d69ae243b20
John <jclry76@gmail.com>
2021-10-21 22:24:04 +0100

    Put chart in functin and add timeout to aloow the value to be got from template - testing solution

--- staticfiles/js/pie.js
+++ staticfiles/js/pie.js
@@ -3,87 +3,43 @@
 // https://stackoverflow.com/questions/37292423/chart-js-label-color
 
 
+function chart(){
 var ctx = document.getElementById('co2_doughnut');
 let total_donor_sink = JSON.parse(document.getElementById('d_sink_chart').textContent);
 let chart_sink_kg = total_donor_sink.donor_sink;
 let chart_sink_tonne = parseInt(chart_sink_kg / 1000);
-
-var myChart = new Chart(ctx, {
-  type: 'doughnut',
-  data: {
-      labels: [
-        'Ireland Emissions - Annual (Tonnes)',
-        'Citizen Tree Sink - Annual (Tonnes)',
-      ],
-      datasets: [{
-        label: 'Citizen Tree Sequestration',
-        data: [60000000, chart_sink_tonne],
-        backgroundColor: [
-          'rgb(250, 250, 250)',
-          'rgb(100, 0, 0)',
+  var myChart = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+        labels: [
+          'Ireland Emissions - Annual (Tonnes)',
+          'Citizen Tree Sink - Annual (Tonnes)',
         ],
-        hoverOffset: 4,
-        cutout: '75%',
-      }]
-  },
-  options: {
-    plugins: {
-      legend: {
-        display: true,
-        labels: {
-          color: "white",
-          font: {
-            size: 20,
+        datasets: [{
+          label: 'Citizen Tree Sequestration',
+          data: [60000000, chart_sink_tonne],
+          backgroundColor: [
+            'rgb(250, 250, 250)',
+            'rgb(100, 0, 0)',
+          ],
+          hoverOffset: 4,
+          cutout: '75%',
+        }]
+    },
+    options: {
+      plugins: {
+        legend: {
+          display: true,
+          labels: {
+            color: "white",
+            font: {
+              size: 20,
+            }
           }
         }
       }
     }
-  }
-});
+  });
 
-
-/* let total_donor_sink = JSON.parse(document.getElementById('d_sink_chart').textContent);
-let chart_sink_kg = total_donor_sink.donor_sink;
-let chart_sink_tonne = parseInt(chart_sink_kg / 1000);
-
-let data = {
-  labels: [
-    'Ireland Emissions - Annual (Tonnes)',
-    'Citizen Tree Sink - Annual (Tonnes)',
-  ],
-  datasets: [{
-    label: 'Citizen Tree Sequestration',
-    data: [60000000, chart_sink_tonne],
-    backgroundColor: [
-      'rgb(250, 250, 250)',
-      'rgb(100, 0, 0)',
-    ],
-    hoverOffset: 4,
-    cutout: '75%',
-  }],
-};
-// https://www.chartjs.org/docs/latest/getting-started/usage.html
-// https://www.chartjs.org/docs/latest/configuration/legend.html
-// https://stackoverflow.com/questions/37292423/chart-js-label-color
-let config = {
-  type: 'doughnut',
-  data: data,
-  options: {
-    plugins: {
-      legend: {
-        display: true,
-        labels: {
-          color: "white",
-          font: {
-            size: 20,
-          }
-        }
-      }
-    }
-  }
-};
-
-let myChart = new Chart(
-  document.getElementById('co2_doughnut'),
-  config
-); */
+}
+setTimeout(() => chart(), 1500); //Test - Give time for page content to load so that the function can get the value from template
